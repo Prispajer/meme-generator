a064be9f296ae6e5742c128acc686e4f4c05793a
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2023-12-11 18:48:29 +0100

    Added random picture

--- App.js
+++ App.js
@@ -3,12 +3,6 @@ import Header from "./components/Header";
 import Inputs from "./components/Inputs";
 
 export default function App() {
-  //   const getRandomImg = () => {
-  //     let memesDataArray = memesData.data.memes;
-  //     let randomImg = Math.floor(Math.random() * memesDataArray.length);
-  //     const url = memesDataArray[randomImg].url;
-  //     console.log(randomImg);
-  //   };
   return (
     <div>
       <Header />
--- components/Inputs.js
+++ components/Inputs.js
@@ -1,20 +1,31 @@
 import React from "react";
+import memesData from "../memesData";
 
 const Inputs = () => {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
       <form>
-        <section>
+        <section className="input-write --left">
           <p>Top text</p>
           <input type="text" placeholder="Write something" />
         </section>
-        <section>
+        <section className="input-write --right">
           <p>Bottom text</p>
           <input type="text" placeholder="Write something" />
         </section>
       </form>
-      <button>Get a new meme image 🖼</button>
-      <section className="button-container"></section>
+      <section className="button-container">
+        <button onClick={getMemeImage}>Get a new meme image 🖼 </button>
+      </section>
+      <img className="meme-image" src={memeImage} alt="random" />
     </main>
   );
 };
